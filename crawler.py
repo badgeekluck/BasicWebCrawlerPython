327eab37a3e3b0e039fee9a50252e957e91c693c
harunbas141 <harun.1996.187@gmail.com>
2017-02-24 02:20:49 +0300

    Updated

--- crawler.py
+++ crawler.py
@@ -1,8 +1,8 @@
-page =('<div id="top_bin"><div id="top_content" class="width960">'
-'<div class="udacity float-left"><a href="http://udacity.com">')
+page =('<div id="bin"><div id="content" class="width960">'
+'<div class="udacity float-left"><a href="http://www.google.com">')
 
-start_link = page.find('<a href=')
-start_quote = page.find('"',start_link)
+first_link = page.find('<a href=')
+start_quote = page.find('"',first_link)
 end_quote = page.find('"',start_quote+1)
 url = page[start_quote+1:end_quote]
 print (url)
