cc148ce8ad3e608562675ba7962b472c3e9d664a
harunbas141 <harun.1996.187@gmail.com>
2017-03-08 13:08:45 +0300

    updated

--- crawler.py
+++ crawler.py
@@ -7,14 +7,14 @@ from bs4 import BeautifulSoup
 r = requests.get('http://www.imdb.com/title/tt0111161/')
 
 b = BeautifulSoup(r.text)
-b.find_all(name='span', attrs={'class':'itemprop', 'itemprop':'name'})    # too many results
-x = b.find_all(name='span', attrs={'class':'itemprop', 'itemprop':'name'})    # too many results
+b.find_all(name='span', attrs={'class':'itemprop', 'itemprop':'name'})      #get  name from itemprop all names
+x = b.find_all(name='span', attrs={'class':'itemprop', 'itemprop':'name'})
 print(x)
 print("******************")
-b.find(name='span', attrs={'class':'itemprop', 'itemprop':'name'}) # just get the first)
-y = b.find(name='span', attrs={'class':'itemprop', 'itemprop':'name'})# just get the first
+b.find(name='span', attrs={'class':'itemprop', 'itemprop':'name'})   #first one
+y = b.find(name='span', attrs={'class':'itemprop', 'itemprop':'name'})
 print(y.text)
 print("**************************************")
-az = float(b.find(name='span', attrs={'itemprop':'ratingValue'}).text)
+az = float(b.find(name='span', attrs={'itemprop':'ratingValue'}).text) # rating values
 print(az)
 print("*****************")
